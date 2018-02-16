d421505fb83043466a43e759633be95d0bb78939
Ryan Lee <marble92@sbcglobal.net>
2018-02-16 15:13:25 -0600

    homework complete

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# burger
+# Eat the Burger!
+
+## Getting Started
+
+## Screen Shots
+
+![Screen shot](public/assets/images/Index.png)
+Index Page
+
+![Screen shot2](public/assets/images/Form.png)
+Form to create new burgers or sides to 'devour' from drop down menus
+
+![Screen shot3](public/assets/images/Menu.png)
+Menu Page - Displays all available Burgers and Sides
+
+## Technologies used
+
+- Node.js
+- MySQL
+- Handlebars - http://handlebarsjs.com/
+- body-parser NPM Package - https://www.npmjs.com/package/inquirer
+- Express NPM Package - https://www.npmjs.com/package/express
+- Mysql NPM Package - https://www.npmjs.com/package/mysql
+- Handlebars NPM Package - https://www.npmjs.com/package/handlebars
+- Express-handlbars NPM Package - https://www.npmjs.com/package/express-handlebars
+
+### Prerequisites
+
+```
+- Node.js - Download the latest version of Node https://nodejs.org/en/
+- Materialize - Add CDN link http://materializecss.com/getting-started.html
+```
+
+## Built With
+
+* Materialize - Wireframe
+* Sequel Pro
---
+++ db/seeds.sql
@@ -0,0 +1,23 @@
+INSERT INTO burgers (burger_name) VALUES ('Mushroom Swiss Burger');
+INSERT INTO burgers (burger_name) VALUES ('Southwestern Veggie Burger');
+INSERT INTO burgers (burger_name) VALUES ('Spinach Artichoke Turkey Burger');
+INSERT INTO burgers (burger_name) VALUES ('Black & Bleu Burger');
+
+INSERT INTO menu (item) VALUES ('Mushroom Swiss Burger');
+INSERT INTO menu (item) VALUES ('Southwestern Veggie Burger');
+INSERT INTO menu (item) VALUES ('Spinach Artichoke Turkey Burger');
+INSERT INTO menu (item) VALUES ('Grilled Teriyaki Chicken Burger');
+INSERT INTO menu (item) VALUES ('Portobello Bacon Burger');
+INSERT INTO menu (item) VALUES ('Cheeseburger');
+INSERT INTO menu (item) VALUES ('Cowboy Whiskey Burger');
+INSERT INTO menu (item) VALUES ('Garden & shroom Burger');
+INSERT INTO menu (item) VALUES ('Chef Special Burger');
+INSERT INTO menu (item) VALUES ('Monster Burger');
+
+INSERT INTO menu (item, entree) VALUES ('French Fries', 0);
+INSERT INTO menu (item, entree) VALUES ('Cheese Fries', 0);
+INSERT INTO menu (item, entree) VALUES ('Onion Rings', 0);
+INSERT INTO menu (item, entree) VALUES ('Garden Salad', 0);
+INSERT INTO menu (item, entree) VALUES ('Caesar Salad', 0);
+INSERT INTO menu (item, entree) VALUES ('Curly Fries', 0);
+INSERT INTO menu (item, entree) VALUES ('Waffle Fries', 0);
